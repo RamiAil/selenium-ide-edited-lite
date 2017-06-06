02d8147c6da12a16bc77e7c7d24d35c7b930b9fd
rami <eilabouni@gmail.com>
2017-06-06 10:49:37 -0700

    new run2 that uninstalls the old firefox

--- run2.sh
+++ run2.sh
@@ -28,6 +28,13 @@ tar -xjf ./firefox-52.1.2esr.tar.bz2
 pwd=`pwd`
 sudo ln -s `echo $pwd`/firefox/firefox /usr/bin/firefox
 
+echo " "
+echo "Please read before continuing: Now installing Selenium IDE plugin, Firefox will run for the first time to load all the configurations for 10 seconds, then it will be killed to overwrite these configurations. After that firefox will run TWICE and prompt an add on installation window"
+echo 'please press install and if you get a "restart now" message close Firefox and DO NOT press "restart now"'
+
+read -p "Press enter to continue"
+
+
 firefox &
 sleep 10
 killall firefox
@@ -35,11 +42,7 @@ killall firefox
 echo "Disabling the add-on installation signature required so we can install an open source add-on"
 echo 'user_pref("xpinstall.signatures.required", false);' >> ~/.mozilla/firefox/*.default/prefs.js
 
-echo " "
-echo "Please read before continuing: Now installing Selenium IDE plugin, Firefox will run TWICE and prompt an add on installation window"
-echo 'please press install and if you get a "restart now" message close Firefox and DO NOT press "restart now"'
 
-read -p "Press enter to continue"
 
 ./firefox/firefox ./build/ide/main/selenium-ide.xpi
 
